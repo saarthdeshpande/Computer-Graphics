19fce83efa1f28959a3a0dbd229a1ebc2ed3a0ec
Saarth Deshpande <deshpandesaarth@gmail.com>
2020-04-10 21:35:34 +0530

    Assignment 8

---
+++ Assignment 8 - Fractal Patterns using Koch Curves.c
@@ -0,0 +1,56 @@
+#include <GL/glut.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+GLfloat Xold = -0.7, Yold = 0.5;
+
+void kochCurve(GLfloat dir,GLfloat len,GLint iter)
+{
+	GLdouble dirRad = 0.0174533 * dir;
+	GLfloat Xnew = Xold + len * cos(dirRad);
+	GLfloat Ynew = Yold + len * sin(dirRad);
+	if(iter == 0) 
+	{
+		glVertex2f(Xold, Yold);
+		glVertex2f(Xnew, Ynew);
+		Xold = Xnew;
+		Yold = Ynew;
+	}
+	else
+	{
+		iter -= 1;	
+		kochCurve(dir, len, iter);	//draw the four parts of the side _/\_
+		dir += 60.0;
+		kochCurve(dir, len, iter);
+		dir -= 120.0;
+		kochCurve(dir, len, iter);
+		dir += 60.0;
+		kochCurve(dir, len, iter);
+	}
+}
+
+void displayFunction()
+{
+	 glClear(GL_COLOR_BUFFER_BIT);
+	 glBegin(GL_LINES);
+	 glColor3f(1.0, 0.0, 0.0);
+
+	 kochCurve(0.0,0.5,1);
+	 kochCurve(-120.0, 0.5, 1);
+	 kochCurve(120.0,0.5,1);
+
+	 glEnd();
+	 glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(0,0);
+	glutInitWindowSize(500,500);
+	glutCreateWindow("Fractal Patterns using Koch Curves");
+	glutDisplayFunc(displayFunction);
+	glutMainLoop();
+}
